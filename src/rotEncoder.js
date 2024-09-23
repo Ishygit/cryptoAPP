80f05492e029734c041f7af82746074910061403
Ishygit <lennyishmael@gmail.com>
2024-09-22 23:02:35 -0500

    new style

--- src/rotEncoder.js
+++ src/rotEncoder.js
@@ -1,34 +1,31 @@
-class RotEncoder {
-   constructor(key) {
-       // Parse the key for rotation in the constructor
-       this.key = parseInt(key, 10);
-   }
-
-   // Encodes the input string by shifting each character
-   encode(str) {
-       return str.split('').map(char => this._shift(char, this.key)).join('');
-   }
-
-   // Decodes the input string by reversing the shift
-   decode(str) {
-       return str.split('').map(char => this._shift(char, -this.key)).join('');
-   }
-
-   // Shifts characters based on the key, using regular expressions for case detection
-   _shift(char, shiftAmount) {
-       // Check if character is uppercase using regex
-       const isUpperCase = /[A-Z]/.test(char);
-       // Check if character is lowercase using regex
-       const isLowerCase = /[a-z]/.test(char);
-
-       if (isUpperCase) {
-           return String.fromCharCode((char.charCodeAt(0) - 65 + shiftAmount + 26) % 26 + 65);
-       } else if (isLowerCase) {
-           return String.fromCharCode((char.charCodeAt(0) - 97 + shiftAmount + 26) % 26 + 97);
-       }
-       // Non-alphabetic characters remain unchanged
-       return char;
-   }
+function RotEncoder(key) {
+   // Parse the key for rotation in the constructor
+   this.key = parseInt(key, 10);
 }
 
+// Encodes the input string by shifting each character
+RotEncoder.prototype.encode = function(str) {
+   return str.split('').map(char => this._shift(char, this.key)).join('');
+};
+
+// Decodes the input string by reversing the shift
+RotEncoder.prototype.decode = function(str) {
+   return str.split('').map(char => this._shift(char, -this.key)).join('');
+};
+
+RotEncoder.prototype._shift = function(char, shiftAmount) {
+   // Check if character is uppercase or lowercase using regEx
+   const isUpperCase = /[A-Z]/.test(char);
+   const isLowerCase = /[a-z]/.test(char);
+
+   if (isUpperCase) {
+       return String.fromCharCode((char.charCodeAt(0) - 65 + shiftAmount + 26) % 26 + 65);
+   } else if (isLowerCase) {
+       return String.fromCharCode((char.charCodeAt(0) - 97 + shiftAmount + 26) % 26 + 97);
+   }
+   // Non-alphabetic characters remain unchanged
+   return char;
+};
+
+// Export the RotEncoder class
 module.exports = RotEncoder;
--- src/sbsEncoder.js
+++ src/sbsEncoder.js
@@ -1,45 +1,44 @@
-class SbsEncoder {
-   constructor(key) {
-       // Process the key into a map for fast lookups in the constructor
-       this.map = this._createMap(key);
-       this.reverseMap = this._createReverseMap(this.map);  // For decoding
-   }
-
-   // Encodes the input string using substitution
-   encode(str) {
-       return str.split('').map(char => this.map[char] || char).join('');
-   }
-
-   // Decodes the input string using the reverse substitution map
-   decode(str) {
-       return str.split('').map(char => this.reverseMap[char] || char).join('');
-   }
-
-   // Creates a map from the key for substitution using split and regular expressions
-   _createMap(key) {
-       const map = {};
-       key.split(',').forEach(pair => {
-           const [from, to] = pair.split('');
-           if (from && to) {
-               // Regular expressions to handle both upper and lowercase
-               map[from] = to;
-               if (/[a-z]/.test(from)) {
-                   map[from.toUpperCase()] = to.toUpperCase();
-               }
-           }
-       });
-       return map;
-   }
-
-   // Creates a reverse map for decoding purposes
-   _createReverseMap(map) {
-       const reverseMap = {};
-       Object.keys(map).forEach(from => {
-           const to = map[from];
-           reverseMap[to] = from;
-       });
-       return reverseMap;
-   }
+function SbsEncoder(key) {
+   // Process the key into a map for fast lookups in the constructor
+   this.map = this._createMap(key);
+   this.reverseMap = this._createReverseMap(this.map);  // For decoding
 }
 
+// Encodes the input string using substitution
+SbsEncoder.prototype.encode = function(str) {
+   return str.split('').map(char => this.map[char] || char).join('');
+};
+
+// Decodes the input string using the reverse substitution map
+SbsEncoder.prototype.decode = function(str) {
+   return str.split('').map(char => this.reverseMap[char] || char).join('');
+};
+
+// Creates a map from the key for substitution using split and regular expressions
+SbsEncoder.prototype._createMap = function(key) {
+   const map = {};
+   key.split(',').forEach(pair => {
+       const [from, to] = pair.split('');  // Split the pair into two characters
+       if (from && to) {
+           // Add to map both lowercase and uppercase transformations
+           map[from] = to;
+           if (/[a-z]/.test(from)) { 
+               map[from.toUpperCase()] = to.toUpperCase();
+           }
+       }
+   });
+   return map;
+};
+
+// Creates a reverse map for decoding purposes
+SbsEncoder.prototype._createReverseMap = function(map) {
+   const reverseMap = {};
+   Object.keys(map).forEach(from => {
+       const to = map[from];
+       reverseMap[to] = from;  // Reverse the mapping
+   });
+   return reverseMap;
+};
+
+// Exporting using Node.js common module system
 module.exports = SbsEncoder;
