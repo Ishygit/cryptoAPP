9d94da28c0f0ef756786e6ae1ea4141fb7eb8c39
Ishygit <lennyishmael@gmail.com>
2024-10-02 17:39:38 -0500

    1

--- hw.js
+++ hw.js
@@ -3,6 +3,11 @@
 // console.log(testArr[0], testArr[1], testArr[2], testArr[3]);
 // console.log(new Int16Array([32768])[0], new Uint16Array([32768])[0]);
 //these are some of the examples that I am working on as I learn JS
-const foo = new String ('foo');
-console.log(foo);
-console.log(typeof foo);
+// const foo = new String ('foo');
+// console.log(foo);
+// console.log(typeof foo);
+String.prototype.printUpperCase = function() {
+    console.log(this.toUpperCase());
+}
+'hello'.printUpperCase();
+// s.prototype.toUpperCase.call(String());
